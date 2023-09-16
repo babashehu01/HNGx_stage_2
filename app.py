cf09a82cb51e22fc3ca19d15c2f92ffcdd58664f
babashehu01 <shettimababashehu57@gmail.com>
2023-09-16 18:56:47 +0100

    Added file storage

--- app.py
+++ app.py
@@ -1,51 +1,68 @@
-from flask import Flask, request
+import os
+import json
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
+data_file = os.path.join(os.path.dirname(__file__), 'data.json')
 
-# Dummy data
-persons = [
-    {'id': 1, 'name': 'Bashehu'},
-    {'id': 2, 'name': 'Shettima'},
-    {'id': 3, 'name': 'Musti'}
-        ]
+# Function to read data from the file
+def read_data():
+    if os.path.exists(data_file):
+        with open(data_file, 'r') as f:
+            return json.load(f)
+    else:
+        return []
 
-# Displays  all people
+# Function to write data to the file
+def write_data(data):
+    with open(data_file, 'w') as f:
+        json.dump(data, f, indent=4)
+
+# Displays all people
 @app.route('/api/all', methods=['GET'])
 def get_persons():
-    return persons
+    persons = read_data()
+    return jsonify({'persons': persons})
 
 # Display a person
 @app.route('/api/<int:person_id>', methods=['GET'])
 def get_person(person_id):
+    persons = read_data()
     for p in persons:
         if p['id'] == person_id:
-            return p
-    return {'error': 'Person not found'}
+            return jsonify({'person': p})
+    return jsonify({'error': 'Person not found'})
 
 # Create a person
-@app.route('/api', methods=['GET', 'POST'])
+@app.route('/api', methods=['POST'])
 def create_person():
-    new_person = {'id': len(persons)+1, 'name': request.json['name']}
-    persons.append(new_person)
-    return new_person
+    data = read_data()
+    new_person = {'id': len(data) + 1, 'name': request.json['name']}
+    data.append(new_person)
+    write_data(data)
+    return jsonify(new_person)
 
 # Update a person
 @app.route('/api/<int:person_id>', methods=['PUT'])
 def update_person(person_id):
-    for p in persons:
+    data = read_data()
+    for p in data:
         if p['id'] == person_id:
             p['name'] = request.json['name']
-            return p
-    return {'error': 'Person not found'}
-
+            write_data(data)
+            return jsonify(p)
+    return jsonify({'error': 'Person not found'})
 # Delete a person
 @app.route('/api/<int:person_id>', methods=['DELETE'])
 def delete_person(person_id):
-    for p in persons:
+    data = read_data()
+    for p in data:
         if p['id'] == person_id:
-            persons.remove(p)
-            return {'Success': f'{p} is deleted successfully'}
-    return {'error': 'Person not found'}
+            data.remove(p)
+            write_data(data)
+            return jsonify({'success': f'Person with ID {person_id} deleted successfully'})
+    return jsonify({'error': 'Person not found'})
 
 if __name__ == '__main__':
     app.run(debug=True)
+
